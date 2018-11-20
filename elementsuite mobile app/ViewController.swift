f7ee63e3925c68ee02a0706a59592e1c6e4b1595
Marvin <marvin@marvin-the-mac-mini.local>
2018-11-20 09:49:23 +0000

    Initial Commit

---
+++ elementsuite mobile app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elementsuite mobile app
+//
+//  Created by Marvin on 12/11/2018.
+//  Copyright © 2018 Marvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
